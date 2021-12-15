a05d7c111572798d1e93f973008cd2a142515e3f
geowat <ldsxxs@163.com>
2021-12-14 21:35:46 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by GeoWat on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
