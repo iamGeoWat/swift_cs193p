056b29782c4e2d82f39ac8ae32ea4b397b7946ce
geowat <ldsxxs@163.com>
2021-12-15 22:50:43 -0800

    Finish Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: false)
+        }
+        .foregroundColor(.purple)
+        .padding(.horizontal)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill()
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3)
+                Text("🙉")
+            } else {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill()
+            }
+      
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
